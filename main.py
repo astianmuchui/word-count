c488602ce36564f682f17bff1ad0e60e1349ed3e
Sebastian <Sebastianmuchui79@gmail.com>
2021-11-19 17:30:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ words = file.read()
 spaces = words.find(' ')
 if spaces != -1:
     number_spaces = words.count(' ')
-    print('The file has '+str(number_spaces+1) +' words')
+    print('The file has got '+str(number_spaces+1) +' words')
 else:
     number_spaces = 0
 
